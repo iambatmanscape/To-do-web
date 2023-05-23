acfb9ae659f49d51130985de8d1db38c78d68227
iambatmanscape <130301578+iambatmanscape@users.noreply.github.com>
2023-05-23 21:26:50 +0530

    Update to.js

--- to.js
+++ to.js
@@ -1,17 +1,31 @@
 let inp=document.querySelector('#infield');
 let sve=document.querySelector('button');
 let list=document.querySelector('ul');
+
 const todo=()=> {
     let l=Array.from(document.querySelectorAll('li'));
     if(l.length>4) { inp.disabled=true; sve.disabled=true; }
     let chk=document.createElement('input');
     chk.type='checkbox';
+    let btn=document.createElement('button');
+    btn.innerText='del';
     let text=document.createTextNode(`${inp.value}`);
     let lbtxt=document.createElement('label');
     lbtxt.appendChild(chk);
     lbtxt.appendChild(text);
     let listitem=document.createElement('li');
     listitem.append(lbtxt);
+    listitem.append(btn);
     list.appendChild(listitem);
-}
+    btn.addEventListener('click',()=>{
+        list.removeChild(listitem);
+        l = Array.from(document.querySelectorAll('li'));
+    if (l.length < 4) {
+      inp.disabled = false;
+      sve.disabled = false;
+    }
+  });
+    }
+
+
 sve.addEventListener('click',todo);
